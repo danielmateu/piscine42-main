09d94170ed8c073b2cbbfac75fb05577047bdc0d
Daniel Mateu <danielmateu86@gmail.com>
2023-10-19 18:07:30 +0200

    intentando contentar a norminette

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -3,37 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: damateu- <damateu-@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 16:11:15 by damateu-              #+#    #+#             */
-/*   Updated: 2023/10/19 16:11:15 by damateu-             ###   ########.fr       */
+/*   Created: 2023/10/19 16:11:15 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/19 18:07:00 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Escribe una funcion que muestre el caracter usado como parámetro
-
-el prototipo de la funcion es el siguiente:
-
-void ft_putchar(char c);
-
-Para mostrar el carácter, debes usar la función write de la siguiente manera:
-
-write(1, &c, 1);
-*/
-
-
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void	ft_putchar(char c)
+{
     write(1, &c, 1);
 }
 
-int main(void) {
-    ft_putchar('h');
-    return 0;
+int	main(void)
+{
+    ft_putchar('a');
+    return (0);
 }
-
-
-
-
