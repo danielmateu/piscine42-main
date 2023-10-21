f07d872c7ac9461e706b6da716814e77e2220ac0
Daniel Mateu <danielmateu86@gmail.com>
2023-10-21 21:35:41 +0200

    cambios en ex03

--- shell01/ex01/print_groups.sh
+++ shell01/ex01/print_groups.sh
@@ -1,9 +1,3 @@
-# Escribe una línea de comandos que muestre, separados por comas y sin espacios, la lista de grupos a la que el login, contenido en la variable de entorno FT_USER, es miembro.
+# id -Gn | tr ' ' ',' | tr -d '\n'
 
-#!/bin/sh
-
-# en el que se usa el comando groups para mostrar los grupos a los que pertenece el usuario, luego se usa el comando tr para reemplazar los espacios por comas y eliminar los saltos de línea, y finalmente se usa el comando cat con la opción -e para mostrar un $ al final de la línea.
-
-id -Gn | tr ' ' ',' | tr -d '\n'
-
-# en el que se usa el comando id con la opción -Gn para mostrar los grupos a los que pertenece el usuario, luego se usa el comando tr para reemplazar los espacios por comas.
+id -Gn $FT_USER | tr ' ' ',' | tr -d '\n'
