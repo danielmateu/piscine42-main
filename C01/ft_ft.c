8ac4798054198d249341867817749ecd1f57b218
daniel_mateu <danielmateu86@gmail.com>
2023-10-23 16:08:20 +0200

    ex01 added

---
+++ C01/ft_ft.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 15:55:39 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/23 16:01:15 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Escribe una funcion que reciba como parámetro un puntero a int, y ponga el valor de este int a 42.
+
+#include <unistd.h>
+
+void ft_ft(int *nbr) {
+    *nbr = 42;
+}
+
+
