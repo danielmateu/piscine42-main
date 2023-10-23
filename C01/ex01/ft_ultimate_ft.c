08ddb1b82c2fad249303ec94534c4a1bd9765938
daniel_mateu <danielmateu86@gmail.com>
2023-10-23 16:44:46 +0200

    meeeh

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 16:20:20 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/23 16:22:00 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Escribe una funcion que reciba como parametro a un puntero a un puntero a un puntero a un puntero a un puntero a un puntero a un int y de al int el valor de 42.
+
+void	ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
