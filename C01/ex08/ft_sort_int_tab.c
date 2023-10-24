abbda1d0b050ad2fabd9147d5731b26e1468a0d4
daniel_mateu <danielmateu86@gmail.com>
2023-10-24 16:35:40 +0200

    ex08 updated

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 16:14:54 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/24 16:31:34 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    Escribe una funcion que ordene un array de int en orden ascendente.
+
+    Los parametros son un puntero a int y el numero de int en el array
+*/
+
+#include <unistd.h>
+
+void ft_sort_int_tab(int *tab, int size)
+{
+    int i;
+    int j;
+    int temp;
+
+    i = 0;
+    while (i < size)
+    {
+        j = i + 1;
+        while (j < size)
+        {
+            if (tab[i] > tab[j])
+            {
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+            j++;
+        }
+        i++;       
+    }
+}
+
+
+
+
