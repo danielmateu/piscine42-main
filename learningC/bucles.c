aae5579220b81d4cd51c458e4c625d83b793243c
daniel_mateu <danielmateu86@gmail.com>
2023-10-25 12:00:54 +0200

    bucles y condicionales

--- learningC/bucles.c
+++ learningC/bucles.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   condicionales.c                                    :+:      :+:    :+:   */
+/*   bucles.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 10:19:16 by damateu-          #+#    #+#             */
-/*   Updated: 2023/10/25 11:45:38 by damateu-         ###   ########.fr       */
+/*   Created: 2023/10/25 11:47:05 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/25 11:59:54 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void imprimir_dia(int numero_dia)
-{
-    switch(numero_dia){
-        case 1:
-            write(1, "Lunes", 5);
-            break;
-        case 2:
-            write(1, "Martes", 6);
-            break;
-        default:
-            write(1, "Are you sure?",13);
-    }   
-}
-
 int main()
 {
-    imprimir_dia(4);
-}
+    int i;
+
+    i = 0;
+    do{
+        write(1, "Hey!\n", 5);
+        i++;
+    }while(i <= 10);
+
+    write(1, "I'm a looser", 12);
+};
