17cd3499f3b3babe78a9607c28f3e34a979777c3
daniel_mateu <danielmateu86@gmail.com>
2023-10-25 16:06:07 +0200

    ex00 updated

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 15:48:35 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/25 16:05:28 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    Reproduce el comportamiento de la funcion strcpy
+
+    La funcion strcpy copia la cadena de caracteres apuntada por src, incluyendo el caracter nulo final, al array de caracteres apuntado por dest.
+
+    La funcion strcpy devuelve un puntero al array de caracteres apuntado por dest.
+
+*/
+
+char    *ft_strcpy(char *dest, char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    /* The line `dest[i] = '\0';` is adding a null character at the end of the copied string in the
+    `dest` array. This is necessary to indicate the end of the string and ensure that it is properly
+    terminated. */
+    dest[i] = '\0';
+    return (dest);
+}
+
+/*
+    #include <stdio.h>
+    #include <string.h>
+
+    int main(void)
+    {
+        char src[] = "Hello World!";
+        char dest[20];
+
+        ft_strcpy(dest, src);
+        printf("%s\n", dest);
+        return (0);
+    }
+*/
