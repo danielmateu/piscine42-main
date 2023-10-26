8968bce828d7096ea70d45e5dd2ae6d938dc9272
Daniel Mateu <danielmateu86@gmail.com>
2023-10-26 23:38:48 +0200

    ex00 updated

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,9 +6,13 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 23:07:06 by damateu-          #+#    #+#             */
-/*   Updated: 2023/10/26 23:09:15 by user             ###   ########.fr       */
+/*   Updated: 2023/10/26 23:38:28 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+    La función strcmp compara dos cadenas de caracteres y devuelve un valor entero que indica si son iguales o cuál es la cadena que es mayor en orden lexicográfico. Si las cadenas son iguales, devuelve 0. Si la primera cadena es mayor que la segunda, devuelve un valor positivo. Si la segunda cadena es mayor que la primera, devuelve un valor negativo.
+*/
 
 int	ft_strcmp(char *s1, char *s2)
 {
