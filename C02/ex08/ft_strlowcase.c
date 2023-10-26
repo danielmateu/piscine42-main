26fb0dcab0789ef938872d972b81c8cf9753f752
daniel_mateu <danielmateu86@gmail.com>
2023-10-26 15:32:55 +0200

    updating things...

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -1,44 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 14:28:48 by damateu-          #+#    #+#             */
-/*   Updated: 2023/10/26 15:09:29 by damateu-         ###   ########.fr       */
+/*   Created: 2023/10/26 15:16:45 by damateu-          #+#    #+#             */
+/*   Updated: 2023/10/26 15:30:57 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-    Escribe una funci'on que muestre uno a uno en pantalla los caracteres de un string. 
-
-    La direccion del primer caracter del string esta incluida en el puntero utilizado como parametro para la funcion
-
-    El prototipo de la funcion debera ser el siguiente:
-
-    void ft_putstr(char *str);
-*/
-
-#include <unistd.h>
-
-void ft_putstr(char *str)
+char *ft_strlowcase(char *str)
 {
     int i;
 
     i = 0;
     while (str[i])
     {
-        write(1, &str[i], 1);
+        if ((str[i] >= 65) && (str[i] <= 90))
+            str[i] = str[i] + 32;
         i++;
     }
+    return (str);
 }
+
+#include <stdio.h>
 
 int main(void)
 {
-    char *str;
-
-    str = "Adios mundo";
-    ft_putstr(str);
+    char str[] = "Hello World";
+    printf("%s", ft_strlowcase(str));
     return (0);
 }
+
