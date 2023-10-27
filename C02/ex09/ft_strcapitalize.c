b97b2345fb9937c5a970967c62278f0d98852306
daniel_mateu <danielmateu86@gmail.com>
2023-10-27 14:04:39 +0200

    fixing issues

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:51:13 by damateu-          #+#    #+#             */
-/*   Updated: 2023/10/26 17:20:27 by damateu-         ###   ########.fr       */
+/*   Updated: 2023/10/27 13:46:48 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,10 @@ char	*ft_strcapitalize(char *str)
 
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			/* The code `if (i == 0)` checks if the current character `str[i]` is the first character of the
-			string. If it is, then it means that it is the first word of the string, so it should be
-			capitalized. Therefore, the code `str[i] = str[i] - 32;` converts the lowercase letter to
-			uppercase. */
 			if (i == 0)
 			{
 				str[i] = str[i] - 32;
 			}
-
-			/* The condition `if (str[i - 1] >= ' ' && str[i - 1] <= '/')` checks if the character before the
-			current character `str[i]` is a space or a punctuation mark. If it is, then it means that the
-			current character `str[i]` is the first character of a new word, so it should be capitalized.
-			Therefore, the code `str[i] = str[i] - 32;` converts the lowercase letter to uppercase. */
 			if (str[i - 1] >= ' ' && str[i - 1] <= '/')
 			{
 				str[i] = str[i] - 32;
