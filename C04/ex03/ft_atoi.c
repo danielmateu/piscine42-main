97b502d1c6e4af98d82b9fb19b1a3d770b7bfba9
daniel_mateu <danielmateu86@gmail.com>
2023-11-02 12:07:20 +0100

    ex03 updated

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,27 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:21:45 by damateu-          #+#    #+#             */
-/*   Updated: 2023/11/02 11:21:46 by damateu-         ###   ########.fr       */
+/*   Updated: 2023/11/02 11:36:55 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+    Escribe una funcion que convierta el principio del string apuntado por str en un entero de tipo int.
+
+    str puede empezar con un n'umero arbitrario de espacios.
+
+    str puede ir seguido de un numero arbitrario de signos + y -. El signo - hara cambiar el signo del enero devuelto en funcion del numero de signos - y si este es par o impar. Si hay un numero impar de signos -, la funcion devolvera un numero negativo. Si hay un numero par de signos -, la funcion devolvera un numero positivo.
+
+    str puede ir seguido de cualquier cantidad de numeros de digitos en base 10
+
+    La funcion tendra que leer los caracteres de str, siempre que estos cumplan con las reglas anteriores y tendra que devolver el numero encontrado hasta entonces
+
+    No se tendran en cuenta los desbordamientos, en estos casos el resultado se considerara indefinido
+
+    ejemplo: ./a.out " ---+--+1234ab567" devolvera -1234
+*/
+
+int ft_atoi(char *str)
+{
+    
+}
