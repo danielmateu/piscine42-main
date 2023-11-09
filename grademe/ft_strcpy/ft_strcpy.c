6521d296fabe377de5417a15fe031c79de863f63
daniel_mateu <danielmateu86@gmail.com>
2023-11-09 14:18:06 +0100

    grademe exercices updated

--- grademe/ft_strcpy/ft_strcpy.c
+++ grademe/ft_strcpy/ft_strcpy.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   only_a.c                                           :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 13:58:13 by damateu-          #+#    #+#             */
-/*   Updated: 2023/11/09 13:59:34 by damateu-         ###   ########.fr       */
+/*   Created: 2023/11/09 14:04:44 by damateu-          #+#    #+#             */
+/*   Updated: 2023/11/09 14:17:37 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-    Write a program that displays a 'a' character on the standard output.
-*/
-
-#include <unistd.h>
-
-int main(void){
-    write(1, "a", 1);
-    return 0;
-}
