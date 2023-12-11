f91dcdc1df688d1aadb9bf17afb171102eae5e66
Daniel Mateu <danielmateu86@gmail.com>
2023-12-11 14:49:27 +0100

    ex07 C00 added

---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 14:42:55 by user              #+#    #+#             */
+/*   Updated: 2023/12/11 14:48:26 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    Escribe una funcion que muesre el número que se ha usado como parámetro. La funcion deberá ser capaz de mostrar todos los valores posibles en una variable tipo int.
+*/
+
+#include <unistd.h>
+
+void ft_putnbr(int nb)
+{
+    int div;
+    int mod;
+
+    if (nb < 0)
+    {
+        write(1, "-", 1);
+        nb *= -1;
+    }
+    if (nb < 10)
+    {
+        mod = nb + 48;
+        write(1, &mod, 1);
+    }
+    else
+    {
+        div = nb / 10;
+        mod = nb % 10 + 48;
+        ft_putnbr(div);
+        write(1, &mod, 1);
+    }
+}
+
+int main(void)
+{
+    ft_putnbr(42);
+    return (0);
+}
