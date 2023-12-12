b80f1de1e5c7b7806e797e1def86f1729348d5e2
Daniel Mateu <danielmateu86@gmail.com>
2023-12-12 16:15:28 +0100

    volvemos a los inicios

---
+++ learningC/01_variables_tipos_de_dato/variables_datos.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   variables_datos.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 11:46:42 by user              #+#    #+#             */
+/*   Updated: 2023/12/12 11:54:27 by user             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Prgramacion proecudural
+
+// funcion main. Es la funcion principal del programa, el codigo que haya en main se ejecutara funcion por funcion
+
+// Las funciones devolveran tipos de datos
+
+// Tipos de datos:
+
+// int: numeros enteros 
+// float: numeros decimales. El simbolo decia siempre será un punto
+// char: caracteres. 'a', 'b', 'c' ... 'z', 'A', 'B', 'C' ... 'Z', '0', '1', '2' ... '9' Como referencia se puede usar la tabla ASCII
+// cadena de caracteres: "Hola mundo" "42" "Hola 42" "42 mundo" "Hola 42 mundo"
+// void: vacio. No devuelve nada. Son procedimientos que no devuelven nada
+
+// construcción de una funcion main qe devuelva un entero:
+
+int main(n)
+{
+    return (0);
+}
