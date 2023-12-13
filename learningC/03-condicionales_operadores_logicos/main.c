6d4ca732f15305ac1d124f78cb2b17694f482c43
Daniel Mateu <danielmateu86@gmail.com>
2023-12-13 12:28:25 +0100

    condicionales y operadores logicos

--- learningC/03-condicionales_operadores_logicos/main.c
+++ learningC/03-condicionales_operadores_logicos/main.c
@@ -6,11 +6,44 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 16:44:59 by user              #+#    #+#             */
-/*   Updated: 2023/12/12 16:49:25 by user             ###   ########.fr       */
+/*   Updated: 2023/12/13 12:27:40 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
+
+void dia_semana(int d)
+{
+    switch (d)
+    {
+    case 1:
+        write(1, "Lunes\n", 6);
+        break;
+    case 2:
+        write(1, "Martes\n", 7);
+        break;
+    case 3:
+        write(1, "Miercoles\n",10);
+        break;
+    case 4:
+        write(1, "Jueves\n", 7);
+        break;
+    case 5:
+        write(1, "Viernes\n", 8);
+        break;
+    case 6:
+        write(1, "Sabado\n", 7);
+        break;
+    case 7:
+        write(1, "Domingo\n", 8);
+        break;
+    
+    default:
+        write(1, "Error\n", 6);
+        break;
+    }
+}
 
 
 int main()
@@ -29,5 +62,7 @@ int main()
     {
         printf("x es mayor o igual que 20\n");
     }
+    dia_semana(1);
+    return 0;
 
 }
