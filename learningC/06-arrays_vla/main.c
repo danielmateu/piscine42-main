53bbe11830b765a6fee2f7119853c2bfc93bc5de
Daniel Mateu <danielmateu86@gmail.com>
2023-12-14 14:32:38 +0100

    arrays

--- learningC/06-arrays_vla/main.c
+++ learningC/06-arrays_vla/main.c
@@ -2,6 +2,8 @@
 
 // Los arrays son una estructura de datos que nos permiten almacenar varios datos del mismo tipo en una misma variable.
 
+// Los identificadores de los arrays son punteros, es decir, son direcciones de memoria.
+
 // Los arrays se declaran de la siguiente forma:
 
 // tipo nombre_array[tamaño];
@@ -15,13 +17,16 @@
 int main()
 {
     // int edades[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Array de 10 elementos
-    int i, edades[8];
-    for (i = 0; i < 8; i++)
+    int tam;
+    printf("Introduce el tamaño del array: ");
+    scanf("%d", &tam);
+    int i, edades[tam];
+    for (i = 0; i < tam; i++)
     {
         printf("Introduce la edad de la persona %d: ", i + 1);
         scanf("%d", &edades[i]);
     }
-    for (i = 0; i < 8; i++)
+    for (i = 0; i < tam; i++)
     {
         printf("La edad de la persona %d es %d\n", i + 1, edades[i]);
     }
