767dfcf4774ea59eae05a8b2fced7f4d587944da
Daniel Mateu <danielmateu86@gmail.com>
2024-01-02 18:33:46 +0100

    ex14 added

---
+++ PiscineReloaded/ex14/ft_sqrt.c
@@ -0,0 +1,30 @@
+
+
+/*
+    Create a function that returns the square root of a number (if it exists), or 0 if the square root is an irrational number.
+
+    Here's how it should be prototyped :
+        int ft_sqrt(int nb);
+*/
+
+#include <stdio.h>
+
+int ft_sqrt(int nb)
+{
+    int i;
+
+    i = 1;
+    while (i * i <= nb)
+    {
+        if (i * i == nb)
+            return (i);
+        i++;
+    }
+    return (0);
+}
+
+int main (void)
+{
+    printf("%d\n", ft_sqrt(100));
+    return (0);
+}
