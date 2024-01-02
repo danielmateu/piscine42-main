a16ab4f00596b1cdfc51106c2f5ab2c72007d4f5
Daniel Mateu <danielmateu86@gmail.com>
2024-01-02 18:30:26 +0100

    ex13 added

---
+++ PiscineReloaded/ex13/ft_recursive_factorial.c
@@ -0,0 +1,27 @@
+
+
+/*
+    Create a recursive function that returns the factorial of the number given as a parameter.
+
+    If there's an error, the function should return 0.
+
+    Here's how it should be prototyped :
+        int ft_recursive_factorial(int nb);
+*/
+
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+    if (nb < 0)
+        return (0);
+    if (nb == 0)
+        return (1);
+    return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int main (void)
+{
+    printf("%d\n", ft_recursive_factorial(5));
+    return (0);
+}
