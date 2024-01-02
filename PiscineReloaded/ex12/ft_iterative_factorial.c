1de569a1eab6c94229217068131b9eddfebff7d0
Daniel Mateu <danielmateu86@gmail.com>
2024-01-02 18:26:46 +0100

    ex12 added

---
+++ PiscineReloaded/ex12/ft_iterative_factorial.c
@@ -0,0 +1,36 @@
+
+
+/*
+    Create an iterated function that returns a number. This number is the result of a
+factorial operation based on the number given as a parameter.
+    
+    If there’s an error, the function should return 0.
+    
+    Here’s how it should be prototyped :
+        int ft_iterative_factorial(int nb);
+*/
+
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb)
+{
+    int i;
+    int result;
+
+    i = 1;
+    result = 1;
+    if (nb < 0)
+        return (0);
+    while (i <= nb)
+    {
+        result *= i;
+        i++;
+    }
+    return (result);
+}
+
+int main (void)
+{
+    printf("%d\n", ft_iterative_factorial(10));
+    return (0);
+}
