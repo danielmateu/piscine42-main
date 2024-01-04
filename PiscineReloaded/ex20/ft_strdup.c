3e1ea339ebc1f54450b87f6910d972825111d112
daniel_mateu <danielmateu86@gmail.com>
2024-01-04 13:44:47 +0100

    ex20 added

---
+++ PiscineReloaded/ex20/ft_strdup.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 17:30:49 by damateu-          #+#    #+#             */
+/*   Updated: 2024/01/04 13:40:58 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Reproduce the behavior of the function strdup (man strdup).
+// Here's how it should be prototyped :
+//  char *ft_strdup(char *src);
+// Allowed functions : malloc
+
+#include <stdlib.h>
+
+/*La función ft_strdup toma como entrada un puntero a una cadena de caracteres (char *src).*/
+char *ft_strdup(char *src)
+{
+    /*inicializa una variable entera i a 0 y un puntero a char dest que será la nueva cadena duplicada.*/
+    int i;
+    char *dest;
+
+    i = 0;
+    /*La función entra en un bucle while para calcular la longitud de la cadena de entrada src. Recorre la cadena src hasta que encuentra el carácter nulo de terminación ('\0'), incrementando i en cada iteración.*/
+    while (src[i])
+        i++;
+
+    /*Una vez que se ha determinado la longitud de la cadena, la función asigna memoria para la nueva cadena dest utilizando la función malloc. El tamaño de la memoria asignada es igual a la longitud de la cadena original más uno para el carácter nulo de terminación ('\0').*/
+    dest = (char *)malloc(sizeof(char) * (i + 1));
+    /*reinicia i a 0 y entra en otro bucle while para copiar los caracteres de la cadena original src a la nueva cadena dest. Recorre la cadena src de nuevo, asignando cada carácter a la posición correspondiente en dest, hasta que encuentra el carácter nulo de terminación ('\0').*/
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    /*Después de copiar todos los caracteres, la función añade un carácter nulo de terminación al final de la cadena dest.*/
+    dest[i] = '\0';
+    /*Finalmente, ft_strdup devuelve un puntero a la nueva cadena duplicada dest.*/
+    return (dest);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "Hello World!";
+    char *str2;
+
+    str2 = ft_strdup(str);
+    printf("%s\n", str2);
+    return (0);
+}
+
+
+
