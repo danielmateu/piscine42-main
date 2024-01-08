e62285ce50b4e9989ae8abf19a765c6686ba8a7d
Daniel Mateu <danielmateu86@gmail.com>
2024-01-08 15:43:16 +0100

    Manipulacion de bits updated

--- learningC/15-manipulacion_de_bits/main.c
+++ learningC/15-manipulacion_de_bits/main.c
@@ -2,5 +2,29 @@
 
 int main()
 {
-    int x = 45;
+    unsigned char a = 185, b = 42;
+
+    printf("a = %u\n", a);
+    printf("b = %u\n", b);
+
+    // NOT(x)
+    // not de foo
+    // foo = ~foo;
+    printf("el not de a = %u\n", (unsigned char)~a);
+    // imprimir el not de foo
+
+    //  AND(x, y)
+    // and de foo
+    // foo = foo & 0x0F;
+    printf("a AND b = %u\n", (unsigned char)(a & b));
+
+    // OR(x, y)
+    // or de foo
+    // foo = foo | 0x0F;
+    printf("a OR b = %u\n", (unsigned char)(a | b));
+
+    // XOR(x, y)
+    // xor de foo
+    // foo = foo ^ 0x0F;
+    printf("a XOR b = %u\n", (unsigned char)(a ^ b));
 }
